ab651a1294cd3af05031c56cace211dcae7951d5
Marketa Majerova <marketamajerova.6@gmail.com>
2022-03-02 22:56:57 +0100

    Changing pictures according to directions.

--- main.js
+++ main.js
@@ -5,22 +5,33 @@ let startPoziceY = panacek.style.top = '300px';
 function stiskSipky(udalost) {
 
 	if (udalost.code === 'ArrowRight') {
+
+		panacek.src = 'obrazky/panacek-vpravo.png';
+		
 		startPoziceX = parseInt(startPoziceX) + 15 + 'px';
 		let novaPoziceX = startPoziceX;
 		panacek.style.left = novaPoziceX;
-		/*
-		let novaPoziceX = parseInt(startPoziceX) + 1 + 'px';
-		panacek.style.left = novaPoziceX;
-		*/
+
 	} else if (udalost.code === 'ArrowLeft') {
+
+		panacek.src = 'obrazky/panacek-vlevo.png';
+		
 		startPoziceX = parseInt(startPoziceX) - 15 + 'px';
 		let novaPoziceX = startPoziceX;
 		panacek.style.left = novaPoziceX;
+		
 	} else if (udalost.code === 'ArrowDown') {
+
+		panacek.src = 'obrazky/panacek.png';
+		
 		startPoziceY = parseInt(startPoziceY) + 15 + 'px';
 		let novaPoziceY = startPoziceY;
 		panacek.style.top = novaPoziceY;
+
 	} else if (udalost.code === 'ArrowUp') {
+
+		panacek.src = 'obrazky/panacek-nahoru.png';
+		
 		startPoziceY = parseInt(startPoziceY) - 15 + 'px';
 		let novaPoziceY = startPoziceY;
 		panacek.style.top = novaPoziceY;
