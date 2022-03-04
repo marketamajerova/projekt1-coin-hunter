79078246f940ba901b812f686a0bb6aee05888d5
Marketa Majerova <marketamajerova.6@gmail.com>
2022-03-04 15:11:21 +0100

    Adding coin and setting random start position

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
 let panacek = document.getElementById('panacek');
-let startPoziceX = panacek.style.left = '600px';
-let startPoziceY = panacek.style.top = '300px';
+let poziceX = panacek.style.left = '500px';
+let poziceY = panacek.style.top = '400px';
+
+let platnoSirka = window.innerWidth;
+let platnoVyska = window.innerHeight;
+
+let mince = document.getElementById('mince');
+let minceVyska = mince.style.height;
+
+let mincePoziceX = mince.style.left = Math.random() * (platnoSirka - minceVyska) + 'px';
+console.log(mincePoziceX);
+let mincePoziceY = mince.style.top = Math.random() * (platnoVyska - minceVyska) + 'px';
+console.log(mincePoziceY);
+
 
 function stiskSipky(udalost) {
 
 	if (udalost.code === 'ArrowRight') {
 
 		panacek.src = 'obrazky/panacek-vpravo.png';
-		
-		startPoziceX = parseInt(startPoziceX) + 15 + 'px';
-		let novaPoziceX = startPoziceX;
+		  
+		poziceX = parseInt(poziceX) + 15 + 'px';
+		let novaPoziceX = poziceX;
 		panacek.style.left = novaPoziceX;
+
+		//if (panacek.style.left === platnoSirka ){
+		//		novaPoziceX = platnoSirka;
+		//	} 
 
 	} else if (udalost.code === 'ArrowLeft') {
 
 		panacek.src = 'obrazky/panacek-vlevo.png';
 		
-		startPoziceX = parseInt(startPoziceX) - 15 + 'px';
-		let novaPoziceX = startPoziceX;
+		poziceX = parseInt(poziceX) - 15 + 'px';
+		let novaPoziceX = poziceX;
 		panacek.style.left = novaPoziceX;
 		
 	} else if (udalost.code === 'ArrowDown') {
 
 		panacek.src = 'obrazky/panacek.png';
 		
-		startPoziceY = parseInt(startPoziceY) + 15 + 'px';
-		let novaPoziceY = startPoziceY;
+		poziceY = parseInt(poziceY) + 15 + 'px';
+		let novaPoziceY = poziceY;
 		panacek.style.top = novaPoziceY;
 
 	} else if (udalost.code === 'ArrowUp') {
 
 		panacek.src = 'obrazky/panacek-nahoru.png';
 		
-		startPoziceY = parseInt(startPoziceY) - 15 + 'px';
-		let novaPoziceY = startPoziceY;
+		poziceY = parseInt(poziceY) - 15 + 'px';
+		let novaPoziceY = poziceY;
 		panacek.style.top = novaPoziceY;
 	}
+
 }
 
 // toto budeš potřebovat později
