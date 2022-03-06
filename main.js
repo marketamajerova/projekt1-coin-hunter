32a8b016b8cac112f21691663e3a28e49a2184b6
Marketa Majerova <marketamajerova.6@gmail.com>
2022-03-06 11:23:42 +0100

    Implementing IF statement about monter's and coin's overlap

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
+// panacek
 let panacek = document.getElementById('panacek');
 let poziceX = panacek.style.left = '500px';
 let poziceY = panacek.style.top = '400px';
+let panacekSirka = panacek.clientWidth;
+let panacekVyska = panacek.clientHeight;
 
+// platno
 let platnoSirka = window.innerWidth;
 let platnoVyska = window.innerHeight;
 
+// mince
 let mince = document.getElementById('mince');
-let minceVyska = mince.style.height;
-
-let mincePoziceX = mince.style.left = Math.random() * (platnoSirka - minceVyska) + 'px';
-console.log(mincePoziceX);
-let mincePoziceY = mince.style.top = Math.random() * (platnoVyska - minceVyska) + 'px';
-console.log(mincePoziceY);
+let minceSirka = mince.clientWidth;
+let mincePoziceX = mince.style.left = Math.random() * (platnoSirka - minceSirka) + 'px';
+let mincePoziceY = mince.style.top = Math.random() * (platnoVyska - minceSirka) + 'px';
 
 
 function stiskSipky(udalost) {
@@ -53,14 +55,19 @@ function stiskSipky(udalost) {
 		panacek.style.top = novaPoziceY;
 	}
 
+	if (!( parseInt(poziceX) + panacekSirka < parseInt(mincePoziceX) || 
+	parseInt(mincePoziceX) + minceSirka < parseInt(poziceX) || 
+	parseInt(poziceY) + panacekVyska < parseInt(mincePoziceY) || 
+	parseInt(mincePoziceY) + minceSirka < parseInt(poziceY))) 
+	{
+		console.log('prekryvame se'); //funguje pouze kdyz se obrazky prekryvaji na zacatku, ne v prubehu hry..
+	}
 }
 
-// toto budeš potřebovat později
-/*
-if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || panacekY + panacekVyska < minceY || minceY + minceVyska < panacekY)) {
-	// panacek a mince se prekryvaji
-}
+
+
+
+// problemy : mince obcas vyskoci z obrazovky, panacek vybiha z obraovky
+/* otazky :
+	1) Proc nefunguje -> panacekVyska = panacek.style.width; ale pouze clientWidth
 */
-
-
-// sem začni psát svůj program
